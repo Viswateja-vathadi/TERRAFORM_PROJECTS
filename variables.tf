ea307bda887c7d55a6af1cf2b055a40ad49463e8
viswateja vathadi <138677061+Viswateja-vathadi@users.noreply.github.com>
2026-08-03 13:23:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "iam_user_name" {
+  description = "IAM User Name"
+  type        = string
+  default     = "admin-user"
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-2"
+}
