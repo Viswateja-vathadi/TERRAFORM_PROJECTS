737eb94ebfe5660a1df3f4a6c350c2283981748c
viswateja vathadi <138677061+Viswateja-vathadi@users.noreply.github.com>
2026-08-03 14:54:12 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -13,18 +13,15 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Create IAM User
 resource "aws_iam_user" "admin_user" {
   name = var.iam_user_name
 }
 
-# Attach Administrator Access Policy
 resource "aws_iam_user_policy_attachment" "admin_access" {
   user       = aws_iam_user.admin_user.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-# Create Access Key
 resource "aws_iam_access_key" "admin_key" {
   user = aws_iam_user.admin_user.name
 }
